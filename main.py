e88031e12bb36668f67222854f3d3d4259c167c3
Johan Hollak <johan.hollak@ut.ee>
2021-11-04 22:01:03 +0200

    let there be sound

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 #initial commit stuff
 import pyaudio
-import numpy
+import numpy as np
 
+FORMAT = pyaudio.paFloat32
+CHANNELS = 1
+RATE = 44100 # muuda vastavalt süsteemile
+
+p = pyaudio.PyAudio()
+stream = p.open(format=FORMAT,
+                channels=CHANNELS,
+                rate=RATE,
+                output=True)
+
+freq = 440
+duration = 2000.0
+
+sound = (np.sin(2*np.pi*np.arange(freq*duration)*freq/RATE)).astype(np.float32)
+
+stream.write(sound)
+
+stream.stop_stream()
+stream.close()
+
+p.terminate()
