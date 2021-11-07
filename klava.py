13c2fc9a793735ca4ce9202bb3218699aedb8076
Reesi Kylmhallik <reesi.kylmhallik@gmail.com>
2021-11-08 00:06:10 +0200

    klava algus

--- klava.py
+++ klava.py
@@ -1,14 +1,21 @@
 import tkinter as tk
 from tkinter.colorchooser import askcolor
-from tkinter import Menu
+from tkinter import *
 
-
-
+effects = {
+    "flat": tk.FLAT,
+    "sunken": tk.SUNKEN,
+    "raised": tk.RAISED,
+    "groove": tk.GROOVE,
+    "ridge": tk.RIDGE,
+}
 aken = tk.Tk()
+
+scales = 3
 
 aken.title('Klahvistik')
 
-akna_laius = 800
+akna_laius =700
 akna_kõrgus = 400
 
 ekraani_laius = aken.winfo_screenwidth()
@@ -22,6 +29,20 @@ aken.geometry(f'{akna_laius}x{akna_kõrgus}+{keskelx}+{keskely}')
 aken.resizable(False, False)
 aken.attributes('-topmost', 1)
 
+valge = 7
+must = [1,1,0,1,1,1,0] * scales
+
+for i in range(valge):
+    tk.Button(aken, bg='White' , command=lambda i=i: clicked('White', i)).grid(row=0, column=i*3, rowspan=2, columnspan=3, ipadx=100, ipady=190, sticky='nsew')
+
+for i in range(valge - 1):
+    if must[i]:
+        tk.Button(aken, bg='black', command=lambda i=i: clicked('Black',i)).grid(row=0, column=(i*3)+2, rowspan=1, columnspan=2, ipadx=30, ipady=120, sticky='n')
+for i in range(valge*3):
+    aken.columnconfigure(i, weight=1)
+
+
+
 menüü = Menu(aken)
 aken.config(menu=menüü)
 
@@ -32,9 +53,11 @@ faili_menüü.add_command(
     command=aken.destroy
 )
 
+
 def muuda_värvi():
     värvid = askcolor(title='Klahvistik')
     aken.configure(bg=värvid[1])
+
 
 faili_menüü.add_command(
     label='Vali värv',
@@ -44,5 +67,11 @@ menüü.add_cascade(
     label='Fail',
     menu=faili_menüü
 )
+
+#7 valget + 5 musta klahvi on vaja (1 oktav)
+
+
+
+
 
 aken.mainloop()
